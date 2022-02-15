7befdb870336a41779ad1a4add68e7d83bfa0f5f
Darío Rumbo <dario.rumbo@gmail.com>
2022-02-14 23:47:26 -0300

    Error handling

--- frontend/src/ethereum.js
+++ frontend/src/ethereum.js
@@ -3,23 +3,24 @@ import PredictionMarket from './contracts/PredictionMarket.json'
 
 const getBlockchain = () => new Promise((resolve, reject) => {
     window.addEventListener('load', async () => {
-        if (window.ethereum) {
-            await window.ethereum.enable()
-            const provider = new ethers.providers.Web3Provider(window.ethereum)
-            const signer = provider.getSigner()
-            const signerAddress = await signer.getAddress()
+        if (!window.ethereum) 
+            return reject("No se encontró wallet.")
 
-            if (!PredictionMarket.networks.hasOwnProperty(window.ethereum.networkVersion))
-                return reject("La red seleccionada es inválida.")
+        await window.ethereum.enable()
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const signerAddress = await signer.getAddress()
 
-            const predictionMarket = new Contract(
-                PredictionMarket.networks[window.ethereum.networkVersion].address,
-                PredictionMarket.abi,
-                signer
-            )
+        if (!PredictionMarket.networks.hasOwnProperty(window.ethereum.networkVersion))
+            return reject("La red seleccionada es inválida.")
 
-            resolve({ signerAddress, predictionMarket })
-        }
+        const predictionMarket = new Contract(
+            PredictionMarket.networks[window.ethereum.networkVersion].address,
+            PredictionMarket.abi,
+            signer
+        )
+
+        resolve({ signerAddress, predictionMarket })
     })
 })
 
